de65ab251221b675b453c79b09036aa01746f243
Bruno Zampirom <brunozampirom@outlook.com>
2023-01-23 01:30:16 -0300

    Python lint changes

--- script_get_stock_history.py
+++ script_get_stock_history.py
@@ -1,11 +1,11 @@
-import requests
-import time
 import os
+from datetime import datetime
+import time
 
+import requests
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from datetime import datetime
 
 firebase_admin.initialize_app(
     credentials.Certificate({
@@ -22,22 +22,22 @@ stocks = db.reference('stockFundamentus').get()
 all_stocks = db.reference('stockHistory')
 
 
-def get_information(stockCode):
+def get_information(stock_code):
     try:
         response = requests.get(
-            f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={stockCode}.sa&apikey=YOUR_API_KEY')
+            f'https://www.alphavantage.co/query?function=TIME_SERIES_MONTHLY_ADJUSTED&symbol={stock_code}.sa&apikey=YOUR_API_KEY')
         if response.status_code == 200:
             if 'Monthly Adjusted Time Series' in response.text:
                 return response.json()
-            elif "Invalid API call" in response.text:
+            if "Invalid API call" in response.text:
                 return 'invalid'
         print("Aguardando 60 seg...")
         time.sleep(60)
-        return get_information(stockCode)
-    except expression as identifier:
+        return get_information(stock_code)
+    except:
         print("Aguardando 60 seg...")
         time.sleep(60)
-        return get_information(stockCode)
+        return get_information(stock_code)
 
 # Formata retorno do serviço
 
@@ -61,23 +61,23 @@ def get_historical_information(historical):
 stock_error_list = []
 
 
-def diff_month(d1, d2):
-    return (d1.year - d2.year) * 12 + d1.month - d2.month
+def diff_month(d_1, d_2):
+    return (d_1.year - d_2.year) * 12 + d_1.month - d_2.month
 
 
-def getVariationMonths(historical_doc, toMonth):
-    if len(historical_doc) - 1 < toMonth:
+def get_variation_months(historical_doc, to_month):
+    if len(historical_doc) - 1 < to_month:
         return 0
 
     lastMonthValue = historical_doc[0]['close']
-    firstMonthValue = historical_doc[toMonth - 1]['close']
+    firstMonthValue = historical_doc[to_month - 1]['close']
 
-    lastMonthDate = datetime.strptime(historical_doc[0]['date'], '%Y-%m-%d')
-    todayDate = datetime.today()
+    last_month_date = datetime.strptime(historical_doc[0]['date'], '%Y-%m-%d')
+    today_date = datetime.today()
 
-    monthsDifference = diff_month(todayDate, lastMonthDate)
+    months_difference = diff_month(today_date, last_month_date)
 
-    if monthsDifference > 1:
+    if months_difference > 1:
         return 0
 
     if firstMonthValue > lastMonthValue:
@@ -85,48 +85,48 @@ def getVariationMonths(historical_doc, toMonth):
     return (lastMonthValue / firstMonthValue - 1) * 100
 
 
-def getVolumeMonths(historical_doc, toMonth):
-    if len(historical_doc) - 1 < toMonth:
+def getVolumeMonths(historical_doc, to_month):
+    if len(historical_doc) - 1 < to_month:
         return 0
 
-    volumeAcum = 0
+    volume_acum = 0
 
-    for month in range(0, toMonth):
-        volumeAcum = volumeAcum + historical_doc[month]['volume']
+    for month in range(0, to_month):
+        volume_acum = volume_acum + historical_doc[month]['volume']
 
-    lastMonthDate = datetime.strptime(historical_doc[0]['date'], '%Y-%m-%d')
-    todayDate = datetime.today()
+    last_month_date = datetime.strptime(historical_doc[0]['date'], '%Y-%m-%d')
+    today_date = datetime.today()
 
-    monthsDifference = diff_month(todayDate, lastMonthDate)
+    months_difference = diff_month(today_date, last_month_date)
 
-    if monthsDifference > 1:
+    if months_difference > 1:
         return 0
 
-    return volumeAcum
+    return volume_acum
 
 
-def function_main(stockCode):
-    data = get_information(stockCode)
+def function_main(stock_code):
+    data = get_information(stock_code)
     if data != "invalid":
         historical = data['Monthly Adjusted Time Series']
         historical_doc = get_historical_information(historical)
 
-        variationTwelveMonths = getVariationMonths(historical_doc, 12)
-        variationSixMonths = getVariationMonths(historical_doc, 6)
-        volumeInLastMonth = getVolumeMonths(historical_doc, 1)
+        variation_twelve_months = get_variation_months(historical_doc, 12)
+        variation_six_months = get_variation_months(historical_doc, 6)
+        volume_in_last_month = getVolumeMonths(historical_doc, 1)
 
-        newStock = {
+        new_stock = {
             "historical": historical_doc,
-            "variationTwelveMonths": variationTwelveMonths,
-            "variationSixMonths": variationSixMonths,
-            "volumeInLastMonth": volumeInLastMonth,
+            "variationTwelveMonths": variation_twelve_months,
+            "variationSixMonths": variation_six_months,
+            "volumeInLastMonth": volume_in_last_month,
         }
 
-        all_stocks.child(stockCode).set(newStock)
-        print(f'Recuperou o historico de {stockCode}')
+        all_stocks.child(stock_code).set(new_stock)
+        print(f'Recuperou o historico de {stock_code}')
     else:
-        stock_error_list.append(stockCode)
-        print(f'Recuperou o historico de {stockCode} - error')
+        stock_error_list.append(stock_code)
+        print(f'Recuperou o historico de {stock_code} - error')
 
 
 #################### INICIO ####################
@@ -134,14 +134,14 @@ totalAtivos = len(stocks)
 
 print(f'Recuperou todos ativos os {len(stocks)} ativos')
 
-for stockCode in stocks:
-    function_main(stockCode)
+for stock_code in stocks:
+    function_main(stock_code)
 
-totalAtivosComErro = len(stock_error_list)
+total_ativos_com_erro = len(stock_error_list)
 
-print(f'Lista dos {totalAtivosComErro} ativos com erro:')
+print(f'Lista dos {total_ativos_com_erro} ativos com erro:')
 for stockerror in stock_error_list:
     print(f'{stockerror} - error')
 
 print()
-print(f'Total de ativos com sucesso: {totalAtivos - totalAtivosComErro}')
+print(f'Total de ativos com sucesso: {totalAtivos - total_ativos_com_erro}')
